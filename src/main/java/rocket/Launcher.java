5d2769b470b01255d1ab720e09fb29aed2f23057
Yasunori MAHATA <mahata777@gmail.com>
2024-07-30 23:21:49 +0900

    initial commit

---
+++ src/main/java/rocket/Launcher.java
@@ -0,0 +1,7 @@
+package rocket;
+
+public class Launcher {
+    static void launchRocket(Rocket rocket, LaunchCode launchCode) {
+        rocket.launch();
+    }
+}
---
+++ src/test/java/rocket/LauncherTest.java
@@ -0,0 +1,11 @@
+package rocket;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class LauncherTest {
+    @Test
+    void sample() {
+        assertEquals(1, 1);
+    }
+}
